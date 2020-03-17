90849a8ca0a51bcf3c3ee3d0ef82641658c85ae4
Lime-Lee <prestoferoce@gmail.com>
2020-03-17 20:07:29 +0900

    Modify: controller 기능 구현 중 병합 전 커밋

--- controller/controller.js
+++ controller/controller.js
@@ -6,5 +6,12 @@ const target = {
     productUl: productUl
 }
 
+// selector-money 이름이 이상하니까 바꾸기
+const selectMoney = document.querySelector('.vm-selector-money');
+
 const vmModel = new VmModel();
 const vmView = new VmView(vmModel, target);
+
+selectMoney.addEventListener("onchange", function (event) {
+    console.log('change test');
+});
