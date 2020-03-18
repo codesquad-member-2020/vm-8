0060e7b07a29b1b7f4caea1450d6e9727e4a4e7a
Lime-Lee <prestoferoce@gmail.com>
2020-03-19 08:20:55 +0900

    Feat: 자판기 버튼, 메시지 기능 구현
    - 잔액 없을 때 버튼 누르면 "투입 금액이 부족합니다" 메시지 출력
    - 자판기에 없는 상품 번호 누르면 선택한 번호 초기화
      e.g. 상품이 20번까지 있는데, 21 이상 누르면 번호 초기화. 메시지 출력은 따로 없음
    - VmProductModel, VmProductView 분리
    
    * 상품 선택하면 자판기 투입된 금액, 상품 부분의 렌더가 다시 안 되는 버그가 있음

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,9 @@
 import VmModel from "../vm/vmModel.js";
+import VmProductModel from "../vm/vmProductModel.js"
 import VmView from "../vm/vmView.js";
+import VmProductView from "../vm/vmProductView.js";
 import WalletModel from "../wallet/walletModel.js";
 import WalletView from "../wallet/walletView.js";
-
 
 const walletTotalView = document.querySelector(".wallet-total");
 const walletMoneyQtyView = document.querySelectorAll(".wallet-money-qty");
@@ -25,22 +26,21 @@ const vmTarget = {
     VMMoneyView: VMMoneyView
 }
 
-const vmModel = new VmModel();
+const vmProductModel = new VmProductModel();
+const vmModel = new VmModel(vmProductModel);
 const walletModel = new WalletModel("http://localhost:5500/json/walletMoney.json");
 const vmView = new VmView(vmModel, walletModel, vmTarget);
+const vmProductView = new VmProductView(vmProductModel, vmTarget);
 const walletView = new WalletView(walletModel, walletTarget);
-
 
 walletList.addEventListener("click", function (event) {
     if (event.target.tagName !== "BUTTON") return;
     walletModel.pay(Number(event.target.value));
-    vmModel.checkPrice(walletModel.walletData.VMCash);
+    vmModel.insertMoney(walletModel.walletData.VMCash);
+    vmProductModel.checkPrice(walletModel.walletData.VMCash);
 });
 
-vmSelectNumber.addEventListener("click",function(event){
-    if(event.target.tagName !== "LI") return;
-    let isSelected = false;
-    if(event.target.value=="10") isSelected = true;
-    vmModel.selectNumber(event.target.value,isSelected);
-    
+vmSelectNumber.addEventListener("click", function (event) {
+    if (event.target.tagName !== "LI") return;
+    vmModel.selectNumber(event.target.value, vmProductModel.products);
 });
